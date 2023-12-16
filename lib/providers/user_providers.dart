6e478eccc2f7eef37a3d7b7c42533f1cc871fe49
Mehmet <mehmetaakbaba@gmail.com>
2023-12-16 19:05:29 +0300

    mesaj sayfası null hatası düzeltildi

--- lib/providers/user_providers.dart
+++ lib/providers/user_providers.dart
@@ -6,16 +6,7 @@ class UserProvider with ChangeNotifier {
   User? _user;
   final AuthMethods _authMethods = AuthMethods();
 
-  User get getUser =>
-      _user ??
-      const User(
-          username: '',
-          uid: '',
-          email: '',
-          photoUrl: '',
-          nameAndSurname: '',
-          followers: [],
-          following: []);
+  User get getUser => _user!;
 
   Future<void> refreshUser() async {
     User user = await _authMethods.getUserDetails();
